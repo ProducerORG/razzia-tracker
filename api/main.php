ce1ffb0771dfd985958ca1803bf887b782fef24d
linus-producer <linus@producer.works>
2025-09-29 16:45:07 +0200

    fix

--- api/main.php
+++ api/main.php
@@ -2,8 +2,8 @@
 
 ini_set('display_errors', 0);
 error_reporting(E_ALL);
-ini_set('log_errors', 1);
-ini_set('error_log', __DIR__ . '/../php-error.log');
+//ini_set('log_errors', 1);
+//ini_set('error_log', __DIR__ . '/../php-error.log');
 header("Access-Control-Allow-Origin: *"); // Falls du CORS brauchst
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");

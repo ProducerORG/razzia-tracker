b6ff56a05bed2502e7fac3ff465d23edbb72748d
linus-producer <linus@producer.works>
2025-09-29 16:28:45 +0200

    debug

--- docs/map.js
+++ docs/map.js
@@ -296,6 +296,8 @@ document.getElementById("reportForm").addEventListener("submit", function(e) {
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(payload)
             });
+            const text = await res.text();
+            console.log("RAW Antwort:", text);
 
             spinner.style.display = "none";
 

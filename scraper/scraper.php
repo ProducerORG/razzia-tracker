6219073c3ffc5aabe3f91bc42806ae0c7770baf8
linus-producer <linus@producer.works>
2025-12-05 14:16:16 +0100

    monitor-email an linus wenn ein scraper failed

--- scraper/scraper.php
+++ scraper/scraper.php
@@ -1,4 +1,38 @@
 <?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+function sendFailureMail($scraper, $exitCode, $output = '') {
+    $mail = new PHPMailer(true);
+    try {
+        $mail->isSMTP();
+        $mail->Host = $_ENV['SMTP_HOST'];
+        $mail->SMTPAuth = true;
+        $mail->Username = $_ENV['SMTP_USER'];
+        $mail->Password = $_ENV['SMTP_PASSWORD'];
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = (int)$_ENV['SMTP_PORT'];
+
+        $mail->setFrom($_ENV['SMTP_USER'], 'Razzia-Tracker: Scraper Monitor');
+        $mail->addAddress($_ENV['linus@producer.works']);
+
+        $mail->Subject = "Scraper-Fehler: $scraper (Exit $exitCode)";
+        $mail->Body = "Der Scraper '$scraper' hat sich mit Fehlercode $exitCode beendet.\n\nAusgabe:\n$output";
+
+        $mail->send();
+    } catch (Exception $e) {
+        // hier bewusst kein Abbruch, Logging reicht
+        file_put_contents(__DIR__ . '/mail_error.log', $e->getMessage() . PHP_EOL, FILE_APPEND);
+    }
+}
+
 // Startet jeden Scraper als separaten PHP-CLI-Prozess, um Namenskonflikte zu vermeiden.
 
 $dir = __DIR__;
@@ -24,10 +58,15 @@ foreach ($files as $file) {
     $cmd = escapeshellarg($php) . ' ' . escapeshellarg($path);
 
     // Ausgabe direkt durchreichen
+    ob_start();
     passthru($cmd, $exitCode);
+    $output = ob_get_clean();
+
+    echo $output;
 
     if ($exitCode !== 0) {
         echo "[WARN] Scraper $file beendete sich mit Code $exitCode\n";
+        sendFailureMail($file, $exitCode, $output);
     } else {
         echo "[INFO] Fertig: $file\n\n";
     }
